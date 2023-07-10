7a2f8807d66682a474abd6c3bd0885078bb4ac0a
spooonmn <jools250@gmail.com>
2023-07-10 18:13:57 +0100

    added camera tracking
    camera now stays in between the two players and zooms in and out to fit them both in

---
+++ Assets/MultiTargetCameraScript.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[ RequireComponent(typeof(Camera))]
+public class MultiTargetCameraScript : MonoBehaviour
+{
+
+    public List<Transform> targets; // list of players
+
+    public Vector3 offset;
+    public float smoothTime = .5f;
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+
+
+    private Vector3 velocity;
+    private Camera cam;
+
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        if (targets.Count == 0)
+        {
+            return;
+        }
+
+        Move();
+        Zoom();
+    }
+
+    void Move()
+    {
+        
+        Vector3 centerPoint = GetCenterPoint(); // gets centre point of all players
+
+        Vector3 newPosition = centerPoint + offset; // adds offset
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime); // smoothing
+    }
+
+    void Zoom()
+    {
+        //Debug.Log( GetGreatestDistance() );
+
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView,newZoom,Time.deltaTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var Bounds = new Bounds(targets[0].position, Vector3.zero);
+        for(int i =0; i < targets.Count; i++)
+        {
+            Bounds.Encapsulate(targets[i].position);
+        }
+        return Bounds.size.x;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        // if only one player camera center is player
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        // creates a rectangle around the players
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        // returns the center of the rectangle 
+        return bounds.center; 
+    }
+}
---
+++ Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+    public float moveSpeed;         // Player movement speed
+    public float walkSpeed = 5f;
+    public float runSpeed = 7f;
+    public float jumpForce;         // Initial force applied when jumping
+    public float maxJumpTime = 0.5f;     // Maximum time the player can hold the jump button
+    public float jumpTimeMultiplier;// Multiplier to increase jump force over time
+    public Transform groundCheck;        // Object used to check if the player is touching the ground
+    public LayerMask groundLayer;        // Layer that represents the ground
+
+    private Rigidbody2D rb;              // Player's rigidbody component
+    private bool isJumping = false;      // Flag to track if the player is jumping
+    private bool isGrounded = false;     // Flag to track if the player is grounded
+    private float jumpTime = 0f;         // Time the jump button has been held
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = walkSpeed;
+    }
+
+    private void Update()
+    {
+        // Check if the player is touching the ground
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        // Player movement
+        float moveX = Input.GetAxis("Horizontal2");
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+
+        // Player jumping
+        if (Input.GetButtonDown("Jump2") && isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+            jumpTime = 0f;
+        }
+
+        if (Input.GetButton("Jump2") && isJumping)
+        {
+            if (jumpTime < maxJumpTime)
+            {
+                rb.AddForce(new Vector2(0f, jumpForce * jumpTimeMultiplier), ForceMode2D.Impulse);
+                jumpTime += Time.deltaTime;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump2"))
+        {
+            isJumping = false;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isGrounded)
+        {
+            moveSpeed = runSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            // Perform actions when the left shift button is released
+            moveSpeed = walkSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Reset jump flag when the player lands on a surface
+        if (collision.gameObject.layer == groundLayer)
+        {
+            isJumping = false;
+        }
+    }
+}
