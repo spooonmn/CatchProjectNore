d434ce3ebd08f760cab116e5ebdc7c890716b316
spooonmn <jools250@gmail.com>
2023-07-29 20:25:14 +0100

    fixed Timer
    Timer working

--- Assets/Scripts/CountdownGameTimer.cs
+++ Assets/Scripts/CountdownGameTimer.cs
@@ -1,23 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Profiling.Editor;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro; // Import the TextMeshPro namespace
 
 public class CountdownGameTimer : MonoBehaviour
-{ float currentTime = 0f;
+{
+    float currentTime = 0f;
     float startTime = 180f;
 
-    public Text countdownText;
+    public TextMeshProUGUI countdownText; // Change the type to TextMeshProUGUI
 
     public void Start()
     {
         currentTime = startTime;
+        UpdateCountdownText(); // Update the text initially to show the starting time
     }
+
     private void Update()
     {
-        currentTime -= 1 * Time.deltaTime;
-        print(currentTime);
-        countdownText.text = currentTime.ToString();
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            if (currentTime < 0)
+            {
+                currentTime = 0;
+                // Handle countdown end here (e.g., game over logic)
+            }
+            UpdateCountdownText();
+        }
+    }
+
+    private void UpdateCountdownText()
+    {
+        int minutes = Mathf.FloorToInt(currentTime / 60f);
+        int seconds = Mathf.FloorToInt(currentTime % 60f);
+
+        string timerText = string.Format("{0:0}:{1:00}", minutes, seconds);
+        countdownText.text = timerText;
     }
 }
